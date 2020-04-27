8dfef9918357d698f91d206310a043d46ccf79bc
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-27 17:01:37 -0500

    Specifying string type in command parameter

--- notes-app/app.js
+++ notes-app/app.js
@@ -10,7 +10,8 @@ yargs.command({
     builder: {
         title: {
             describe: 'Note title',
-            demandOption: true
+            demandOption: true,
+            type: 'string'
         }
     },
     handler: function(argv) {
