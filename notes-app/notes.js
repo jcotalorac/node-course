50e1d8efb0e46826b24a84ad4359bf7b03ea9680
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-27 18:35:50 -0500

    Function to load notes

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,9 +1,17 @@
+const fs = require('fs');
+
 const getNotes = function() {
     return 'Your notes...';
 }
 
 const addNote = function(title, body) {
     
+}
+
+const loadNotes = function() {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const data = JSON.parse(dataBuffer.toString);
+    return data;
 }
 
 module.exports = {
