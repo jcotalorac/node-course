d39b9594bd787ae9b9144708bc9d8d36d1aed66d
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-27 16:08:15 -0500

    Yargs input is better structured to read

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,12 @@
 const validator = require('validator');
+const yargs = require('yargs');
 const chalk = require('chalk');
 
-const command = process.argv[2];
-
 console.log(process.argv);
+console.log(yargs.argv);
 
-if(command === 'add'){
+/*if(command === 'add'){
     console.log('Adding note!');
 } else if(command === 'remove') {
     console.log('Removing note!');
-}
+}*/
