fb3a6b1ff58f7260b768ad2a35ab441b05bc53b9
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-27 15:39:30 -0500

    Command arguments are separated by spaces

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,6 +3,8 @@ const chalk = require('chalk');
 
 const command = process.argv[2];
 
+console.log(process.argv);
+
 if(command === 'add'){
     console.log('Adding note!');
 } else if(command === 'remove') {
