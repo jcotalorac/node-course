7101e35d642cb8581e4a178b9777d85519a05fdd
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-27 18:31:31 -0500

    Using notes module to add a note

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,7 @@
 const validator = require('validator');
 const yargs = require('yargs');
 const chalk = require('chalk');
+const notes = require('./notes');
 
 yargs.version('1.1.0');
 
@@ -20,8 +21,7 @@ yargs.command({
         }
     },
     handler: function(argv) {
-        console.log('Title: ', argv.title);
-        console.log('Body: ', argv.body);
+        notes.addNote(argv.title, argv.author);
     }
 });
 
