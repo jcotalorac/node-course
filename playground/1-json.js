860338e508e26eee94ba8511c10de1fb35d8f03e
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-27 17:52:26 -0500

    Parsing string to json

--- playground/1-json.js
+++ playground/1-json.js
@@ -8,4 +8,6 @@ const book = {
 //fs.writeFileSync('1-json.json', bookJSON);
 
 const dataBuffer = fs.readFileSync('1-json.json');
-console.log(dataBuffer.toString());
+const dataJSON = dataBuffer.toString();
+const data = JSON.parse(dataJSON);
+console.log(data.title);
