938c77887f4bc7a4d6a6471f00292a26046084fb
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-27 19:43:58 -0500

    Avoiding duplicates

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -6,12 +6,21 @@ const getNotes = function() {
 
 const addNote = function(title, body) {
     const notes = loadNotes();
-
-    notes.push({
-        title: title,
-        body: body
+    const duplicateNotes = notes.filter(function(note){
+        return note.title === title;
     });
-    saveNotes(notes);
+
+    if(duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        });        
+        saveNotes(notes);
+        console.log('New note added!');
+    } else {
+        console.log('Note title taken!');
+    }
+
 }
 
 const saveNotes = function(notes) {
@@ -22,7 +31,7 @@ const saveNotes = function(notes) {
 const loadNotes = function() {
     try {
         const dataBuffer = fs.readFileSync('notes.json');
-        const data = JSON.parse(dataBuffer.toString);
+        const data = JSON.parse(dataBuffer.toString());
         return data;        
     } catch (error) {
         return [];
