aeeffe42584816e42e63e7356f658a707c84c20f
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-28 14:55:12 -0500

    Enhancing messages

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -6,9 +6,10 @@ const getNotes = (title) => {
     const note = notes.find(note => note.title === title);
 
     if(note) {
-        console.log(chalk.green.inverse('Note found'));
+        console.log(chalk.inverse(note.title));
+        console.log(note.body);
     } else {
-        console.log(chalk.red.inverse('Note not foound'));
+        console.log(chalk.red.inverse('Note not found!'));
     }
 }
 
