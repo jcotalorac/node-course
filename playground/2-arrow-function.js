413afe2c20a0f15da5c8d063e6be566af430cdd9
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-28 11:18:07 -0500

    Arrow functions takes context where is created

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -13,10 +13,9 @@ const event = {
     name: 'Birthday Party',
     guestList: ['Andrew', 'Jen', 'Mike'],
     printGuestList() {
-        const that = this;
         console.log('Guest list for ' + this.name);
-        this.guestList.forEach(function(guest){
-            console.log(guest + ' is attending ' + that.name);
+        this.guestList.forEach((guest) => {
+            console.log(guest + ' is attending ' + this.name);
         });
     }
 };
