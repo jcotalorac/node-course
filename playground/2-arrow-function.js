d158977e9b4d1b438f5d2a86d7f48b8941cd5e73
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-28 11:15:47 -0500

    ES5 functions loses this keyword reference

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -11,8 +11,12 @@
 
 const event = {
     name: 'Birthday Party',
+    guestList: ['Andrew', 'Jen', 'Mike'],
     printGuestList() {
         console.log('Guest list for ' + this.name);
+        this.guestList.forEach(function(guest){
+            console.log(guest + ' is attending ' + this.name);
+        });
     }
 };
 
