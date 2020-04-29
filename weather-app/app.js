5fe2265c473b8966a7c2c20654d91b06e698ef21
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-29 15:26:02 -0500

    Low level error and matching errors handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,6 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=37.8267,-122.4233&units=m';
+/*const url = 'http://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=37.8267,-122.4233&units=m';
 
 request({ url: url, json: true }, (error, response) => {
     
@@ -15,11 +15,17 @@ request({ url: url, json: true }, (error, response) => {
         console.log(`${weatherDescription}. It is currently ${temperature} degress out. It feels like ${feelslike} degress out.`);
 
     }
-});
+});*/
 
-/*const geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamNvdGFsb3JhYyIsImEiOiJjazlsbm8zbXEyN3l5M2ZvMTR4d2pxcGtuIn0.XqG60Bw_1muPAGX4YWwy5A';
+const geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamNvdGFsb3JhYyIsImEiOiJjazlsbm8zbXEyN3l5M2ZvMTR4d2pxcGtuIn0.XqG60Bw_1muPAGX4YWwy5A';
 
 request({ url: geoCodeUrl, json: true}, (error, response) => {
-    const [latitude, longitude] = response.body.features[0].center;
-    console.log(`Lat: ${latitude}, Long: ${longitude}`);
-});*/
+    if(error) {
+        console.log('Unable to connect to geo service');
+    } else if(response.body.features.length === 0) {
+        console.log('Unable to find resulst for query');
+    } else {
+        const [latitude, longitude] = response.body.features[0].center;
+        console.log(`Lat: ${latitude}, Long: ${longitude}`);
+    }
+});
