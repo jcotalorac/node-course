51dae1e23fcd3d56676757037ec385878547e70b
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-29 13:54:01 -0500

    Requesting geocode service

--- weather-app/app.js
+++ weather-app/app.js
@@ -9,3 +9,10 @@ request({ url: url, json: true }, (error, response) => {
     const weatherDescription = response.body.current.weather_descriptions[0];
     console.log(`${weatherDescription}. It is currently ${temperature} degress out. It feels like ${feelslike} degress out.`);
 });
+
+const geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamNvdGFsb3JhYyIsImEiOiJjazlsbm8zbXEyN3l5M2ZvMTR4d2pxcGtuIn0.XqG60Bw_1muPAGX4YWwy5A';
+
+request({ url: geoCodeUrl, json: true}, (error, response) => {
+    const [latitude, longitude] = response.body.features[0].center;
+    console.log(`Lat: ${latitude}, Long: ${longitude}`);
+});
