bc64efa07abd137601dd20e3ac23b9e29e417ca9
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-29 12:03:20 -0500

    Using units and description

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,11 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=37.8267,-122.4233';
+const url = 'http://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=37.8267,-122.4233&units=m';
 
 request({ url: url, json: true }, (error, response) => {
     //console.log(response.body.current);
     const temperature = response.body.current.temperature;
     const feelslike = response.body.current.feelslike;
-    console.log(`It is currently ${temperature} degress out. It feels like ${feelslike} degress out.`);
+    const weatherDescription = response.body.current.weather_descriptions[0];
+    console.log(`${weatherDescription}. It is currently ${temperature} degress out. It feels like ${feelslike} degress out.`);
 });
