86ad91e73f46af42fd2996daef52371d4709fb5c
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-29 11:25:43 -0500

    Configuring json parse response

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,6 @@ const request = require('request');
 
 const url = 'http://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=37.8267,-122.4233';
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.current);
 });
