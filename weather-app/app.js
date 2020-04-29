e0883aa248b862a14f69db7e14318dfdeb1edd7e
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-29 15:04:48 -0500

    Printing intentional error

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,18 +1,19 @@
 const request = require('request');
 
-const url = 'http://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=37.8267,-122.4233&units=m';
+const url = 'http://api.weatherstack.co/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=37.8267,-122.4233&units=m';
 
 request({ url: url, json: true }, (error, response) => {
+    console.log(error);
     //console.log(response.body.current);
-    const temperature = response.body.current.temperature;
+    /*const temperature = response.body.current.temperature;
     const feelslike = response.body.current.feelslike;
     const weatherDescription = response.body.current.weather_descriptions[0];
-    console.log(`${weatherDescription}. It is currently ${temperature} degress out. It feels like ${feelslike} degress out.`);
+    console.log(`${weatherDescription}. It is currently ${temperature} degress out. It feels like ${feelslike} degress out.`);*/
 });
 
-const geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamNvdGFsb3JhYyIsImEiOiJjazlsbm8zbXEyN3l5M2ZvMTR4d2pxcGtuIn0.XqG60Bw_1muPAGX4YWwy5A';
+/*const geoCodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiamNvdGFsb3JhYyIsImEiOiJjazlsbm8zbXEyN3l5M2ZvMTR4d2pxcGtuIn0.XqG60Bw_1muPAGX4YWwy5A';
 
 request({ url: geoCodeUrl, json: true}, (error, response) => {
     const [latitude, longitude] = response.body.features[0].center;
     console.log(`Lat: ${latitude}, Long: ${longitude}`);
-});
+});*/
