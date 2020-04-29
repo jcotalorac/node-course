174adee103e353c282a2a7d9fbe60583253baf1b
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-28 21:54:30 -0500

    Making an http request to an external API

---
+++ weather-app/app.js
@@ -0,0 +1,7 @@
+const request = require('request');
+
+const url = 'http://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=37.8267,-122.4233';
+
+request({ url: url }, (error, response) => {
+    console.log(response);
+});
