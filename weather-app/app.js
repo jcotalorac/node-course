19484a9b86d1141f1d986ecd6d936fbe048ab0d9
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-30 18:08:24 -0500

    Synchronizing operations of request by callback chain

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,9 +4,8 @@ const geocode = require('./utils/geocode');
 geocode('Madelena', (error, data) => {
     console.log('Error', error);
     console.log('Data', data);
-});
-
-forecast(-74.160509, 4.591965, (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
+    forecast(data.latitude, data.longitude, (error, data) => {
+        console.log('Error', error);
+        console.log('Data', data);
+    });
 });
