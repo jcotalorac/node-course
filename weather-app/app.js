446d9e22343df04b29b348e0e824a28c65232407
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-30 18:41:50 -0500

    Arranging chaining callback execution

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,10 +2,13 @@ const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
 geocode('Madelena', (error, data) => {
-    console.log('Error', error);
-    console.log('Data', data);
-    forecast(data.latitude, data.longitude, (error, data) => {
-        console.log('Error', error);
-        console.log('Data', data);
+    if(error) {
+        return console.log(error);
+    }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if(error) {
+            return console.log(error);
+        }
+        console.log('Data', forecastData);
     });
 });
