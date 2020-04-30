e201883459ba1527642adb6cb157843967ac1fff
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-30 18:57:47 -0500

    Validating command input

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,15 +1,19 @@
 const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
-geocode(process.argv[2], (error, data) => {
-    if(error) {
-        return console.log(error);
-    }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+const place = process.argv[2];
+
+if(place) {
+    geocode(process.argv[2], (error, data) => {
         if(error) {
             return console.log(error);
         }
-        console.log(data.location);
-        console.log('Data', forecastData);
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if(error) {
+                return console.log(error);
+            }
+            console.log(data.location);
+            console.log('Data', forecastData);
+        });
     });
-});
+}
