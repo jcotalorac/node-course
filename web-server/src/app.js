d3b588c59c8e25263fa849e270bcf909f474bef9
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-01 12:10:47 -0500

    Routing help page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,6 +6,10 @@ app.get('/', (request, response) => {
     response.send('Hello express');
 });
 
+app.get('/help', (request, response) => {
+    response.send('Help page');
+});
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
 });
