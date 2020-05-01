24f1fee26c2b80e7ca7dcf9a35ec75e4f8c12f94
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-30 22:14:50 -0500

    Destructuring object params

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,16 +6,16 @@ const place = process.argv[2];
 if(!place) {
     console.log('Please provide an address');
 } else {
-    geocode(process.argv[2], (error, data) => {
+    geocode(process.argv[2], (error, { latitude, longitude, location }) => {
         if(error) {
             return console.log(error);
         }
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, {temperature, feelslike, weatherDescription }) => {
             if(error) {
                 return console.log(error);
             }
-            console.log(data.location);
-            console.log('Data', forecastData);
+            console.log(location);
+            console.log(`${weatherDescription}. Temperature is ${temperature} and it feels like ${feelslike}`);
         });
     });
 }
