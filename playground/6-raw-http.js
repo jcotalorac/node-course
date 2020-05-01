c6b16bf5f8f55db2665daaef9aaa7a279515d90b
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-30 23:43:04 -0500

    Emitting end event to detect when all chunks are read

--- playground/6-raw-http.js
+++ playground/6-raw-http.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const url = `http://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=40,-75&units=m`;
 
-http.request(url, (response) => {
+const request = http.request(url, (response) => {
 
     let data = '';
 
@@ -12,3 +12,5 @@ http.request(url, (response) => {
 
     response.on('end', () => {});
 });
+
+request.end();
