166d8d393e595608e0446c5c7d2c73a379dd0147
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-01 12:22:26 -0500

    Sending html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello express');
+    response.send('<h1>Weather</h1>');
 });
 
 app.get('/help', (request, response) => {
