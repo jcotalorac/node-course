947496f5a82ed7233df95891fb777f7d212cd1d4
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-30 22:42:55 -0500

    Destructuring response to get only body

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -4,15 +4,15 @@ const request = require('request');
 const forecast = (longitude, latitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=${longitude},${latitude}&units=m`;
 
-    request({ url, json: true}, (error, response) => {
+    request({ url, json: true}, (error, { body }) => {
         if(error) {
             callback('Unable to connect to weather service!', undefined);
-        } else if(response.body.error) {
+        } else if(body.error) {
             callback('Unable to find location', undefined);
         } else {
-            const temperature = response.body.current.temperature;
-            const feelslike = response.body.current.feelslike;
-            const weatherDescription = response.body.current.weather_descriptions[0];
+            const temperature = body.current.temperature;
+            const feelslike = body.current.feelslike;
+            const weatherDescription = body.current.weather_descriptions[0];
             callback(undefined, { temperature, feelslike, weatherDescription });
         }
     });
