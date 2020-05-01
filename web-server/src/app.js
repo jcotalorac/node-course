20fff2d085ba3fd1bb3fa2caa7a69c6654142602
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-01 12:04:38 -0500

    Running server app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,3 +5,7 @@ const app = express();
 app.get('/', (request, response) => {
     response.send('Hello express');
 });
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
