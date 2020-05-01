9655c37ef8960f7e6479c3191e87823f2478f4f5
juan.otalora <juancarlos.otalora@wnco.com>
2020-04-30 22:59:49 -0500

    Registering event on data obtention

--- playground/6-raw-http.js
+++ playground/6-raw-http.js
@@ -3,5 +3,8 @@ const http = require('http');
 const url = `http://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=40,-75&units=m`;
 
 http.request(url, (response) => {
-    
+
+    response.on('data', (chunk) => {
+        
+    });
 });
