589cbe48dcf72a77761d0e52cf4023bbacbab8b2
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-01 22:27:16 -0500

    Customizing views folder name

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,9 +3,13 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')));
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+app.use(express.static(publicDirectoryPath));
 
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 
 app.get('', (request, response) => {
     response.render('index', {
