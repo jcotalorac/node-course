65185e99138be81921c467b66858913f9eb42a1c
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-02 22:36:16 -0500

    Using fetch library on javascript client side

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,8 @@
 console.log('Client site javascript file is loaded!');
+
+fetch('http://puzzle.mead.io/puzzle').then(response => {
+    response.json().then(data => {
+        console.log(data);
+        
+    });
+});
