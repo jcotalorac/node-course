b4b14fcd1ed178ea3c6dffb3a9a99b991291be75
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-02 23:11:49 -0500

    Fetching weather on form submit

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,17 +1,16 @@
 console.log('Client site javascript file is loaded!');
 
-fetch('http://localhost:3000/weather?address=Madelena').then(response => {
-    response.json().then(data => {
-        if(data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data);
-        }
-    });
-});
-
 const weatherForm = document.querySelector('form');
+
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log('Form submit');
+    fetch('http://localhost:3000/weather?address=Madelena').then(response => {
+        response.json().then(data => {
+            if(data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data);
+            }
+        });
+    });
 });
