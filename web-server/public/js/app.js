285bce8075663f6c33a4fc061bb5295cb95073cd
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-02 23:18:06 -0500

    Capturing input as address search

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,10 +1,11 @@
 console.log('Client site javascript file is loaded!');
 
 const weatherForm = document.querySelector('form');
+const input = document.querySelector('input');
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    fetch('http://localhost:3000/weather?address=Madelena').then(response => {
+    fetch(`http://localhost:3000/weather?address=${input.value}`).then(response => {
         response.json().then(data => {
             if(data.error) {
                 console.log(data.error);
