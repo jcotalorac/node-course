526dbb932281bbc883c1fca7860aca818e1cd720
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-03 00:48:56 -0500

    Visual output of fetching weather results

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,15 +2,20 @@ console.log('Client site javascript file is loaded!');
 
 const weatherForm = document.querySelector('form');
 const input = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
+    messageOne.textContent = '';
+    messageTwo.textContent = '';
     fetch(`http://localhost:3000/weather?address=${input.value}`).then(response => {
         response.json().then(data => {
-            if(data.error) {
-                console.log(data.error);
+            if(data.message) {
+                messageOne.textContent = data.message;
             } else {
-                console.log(data);
+                messageTwo.textContent = `${data.location}, ${data.weatherDescription}`;                
             }
         });
     });
