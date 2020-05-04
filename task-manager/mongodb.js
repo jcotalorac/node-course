b9d0062fea4ac9aa1cc3e2aa58433db5356ba204
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-04 12:06:42 -0500

    Insert many operation

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -10,7 +10,7 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
 
     const db = client.db(databaseName);
-    db.collection('users').insertOne({
+    /*db.collection('users').insertOne({
         name: 'JC',
         age: 37
     }, (error, result) => {
@@ -18,6 +18,20 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
             return console.log('Unable to insert user');
         }
         console.log(result.ops);
-        
+    });*/
+    db.collection('users').insertMany([
+        {
+            name: 'Jen',
+            age: 28
+        },
+        {
+            name: 'Gunther',
+            age: 27
+        }
+    ], (error, result) => {
+        if(error) {
+            return console.log('Unable to insert documents!');
+        }
+        console.log(result.ops);
     });
 });
