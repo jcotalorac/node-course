4e41df417160d32feb9cba52847b80680a7c833b
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-05 09:53:55 -0500

    Required description in task

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -6,7 +6,7 @@ mongoose.connect('mongodb://localhost:27017/task-manager-api', {
     useCreateIndex: true
 });
 
-const User = mongoose.model('User', {
+/* const User = mongoose.model('User', {
     name: {
         type: String,
         required: true,
@@ -48,7 +48,7 @@ const User = mongoose.model('User', {
 const me = new User({
     name: 'JC',
     email: 'mail@mail.com',
-    password: '123456password'
+    password: '123456passwor'
 });
 
 me.save()
@@ -57,20 +57,20 @@ me.save()
 })
 .catch((error => {
     console.log('Error!', error);
-}));
+})); */
 
-/* const Task = mongoose.model('Task', {
+const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 });
 
 const task1 = new Task({
-    description: "Task1",
-    completed: false
 });
 
 task1.save()
@@ -79,4 +79,4 @@ task1.save()
 })
 .catch((error) => {
     console.log('Error!', error);
-}); */
+});
