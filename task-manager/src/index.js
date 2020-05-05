561e885d6a7e468fac7e8d21dd3d7c40e9f5094b
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-05 12:01:21 -0500

    Post request handling

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,3 +6,7 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
+
+app.post('/users', (request, response) => {
+    response.send('testing!');
+});
