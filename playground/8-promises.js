6137a1507825ebe915e95d075b43f2ea4d4bbde3
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-05 18:50:36 -0500

    Nested promises

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -28,6 +28,13 @@ const add = (a, b) => {
 add(1, 2)
 .then((sum) => {
     console.log(sum);
+    add(sum, 5)
+    .then((sum2) => {
+        console.log(sum2);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 })
 .catch((error) => {
     console.log(error);
