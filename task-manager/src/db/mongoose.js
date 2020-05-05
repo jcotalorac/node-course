47fb05e88859741e6ce3cd602ddd8da37b51d314
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-05 09:13:16 -0500

    Using validator in validate funtion of mongoose

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://localhost:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -9,6 +10,15 @@ const User = mongoose.model('User', {
     name: {
         type: String,
         required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
     },
     age: {
         type: Number,
@@ -22,7 +32,7 @@ const User = mongoose.model('User', {
 
 const me = new User({
     name: 'JC',
-    age: '-37'
+    email: '-37'
 });
 
 me.save()
