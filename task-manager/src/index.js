52de8348c47884ae7adf0ebe216205ca978b5bde
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-05 12:07:57 -0500

    Configuring json as a format of body request and responses in server

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log('Server is up on port ' + port);
-});
+app.use(express.json());
 
 app.post('/users', (request, response) => {
     response.send('testing!');
+});
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port);
 });
