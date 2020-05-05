fb8e9ca71b0f3a7fadf8603dd35c742885e4bb9e
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-04 20:28:59 -0500

    Defining a model of user, with string and number typed attributes

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -4,3 +4,12 @@ mongoose.connect('mongodb://localhost:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 });
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+});
