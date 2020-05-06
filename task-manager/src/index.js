85db7fd5f2318f06871193eefe400854dedc04ed
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-06 18:06:52 -0500

    Refactoring to move user router operations

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,86 +2,16 @@ const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user');
 const Task = require('./models/task');
+const userRouter = require('./routers/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-const router = new express.Router();
-router.get('/test', (request, response) => {
-    response.send('This is from another router');
-});
-app.use(router);
+app.use(userRouter);
 
-app.post('/users', async (request, response) => {
-    const user = new User(request.body);
 
-    try {
-        await user.save();
-        response.status(201).send(user);
-    } catch (error) {
-        response.status(400).send(error);
-    }
-});
-
-app.get('/users', async (request, response) => {
-    try {
-        let allUsers = await User.find();
-        const count = await User.countDocuments();
-        allUsers.push(count);
-        response.send(allUsers);
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
-
-app.get('/users/:id', async (request, response) => {
-    try {
-        const user = await User.findById(request.params.id);
-        if(!user) {
-            return response.status(404).send();
-        }
-        response.send(user);
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
-
-app.patch('/users/:id', async (request, response) => {
-    const updates = Object.keys(request.body);
-    const allowedUpdates = ['name', 'email', 'password', 'age'];
-    const isValidUpdating = updates.every(update => allowedUpdates.includes(updates));
-    
-    if(!isValidUpdating) {
-        return response.status(400).send({ error: 'Invalid updates!' });
-    }
-    try {
-        const user = await User.findByIdAndUpdate(request.params.id, request.body, {
-            new: true,
-            runValidators: true
-        });
-        if(!user) {
-            return response.status(404).send();
-        }
-        response.send(user);
-    } catch (error) {
-        response.status(400).send();
-    }
-});
-
-app.delete('/users/:id', async (request, response) => {
-    try {
-        const user = await User.findByIdAndDelete(request.params.id);
-        
-        if(!user) {
-            return response.status(404).send();
-        }
-        response.send(user);
-    } catch (error) {
-        response.send(500).send(error);
-    }
-});
 
 app.post('/tasks', async (request, response) => {
     const task = new Task(request.body);
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -13,4 +13,62 @@ router.post('/users', async (request, response) => {
     }
 });
 
+router.get('/users', async (request, response) => {
+    try {
+        let allUsers = await User.find();
+        const count = await User.countDocuments();
+        allUsers.push(count);
+        response.send(allUsers);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+router.get('/users/:id', async (request, response) => {
+    try {
+        const user = await User.findById(request.params.id);
+        if(!user) {
+            return response.status(404).send();
+        }
+        response.send(user);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+router.patch('/users/:id', async (request, response) => {
+    const updates = Object.keys(request.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidUpdating = updates.every(update => allowedUpdates.includes(update));
+    
+    if(!isValidUpdating) {
+        return response.status(400).send({ error: 'Invalid updates!' });
+    }
+    try {
+        const user = await User.findByIdAndUpdate(request.params.id, request.body, {
+            new: true,
+            runValidators: true
+        });
+        if(!user) {
+            return response.status(404).send();
+        }
+        response.send(user);
+    } catch (error) {
+        response.status(400).send();
+    }
+});
+
+router.delete('/users/:id', async (request, response) => {
+    try {
+        const user = await User.findByIdAndDelete(request.params.id);
+        
+        if(!user) {
+            return response.status(404).send();
+        }
+        response.send(user);
+    } catch (error) {
+        response.send(500).send(error);
+    }
+});
+
 module.exports = router;
