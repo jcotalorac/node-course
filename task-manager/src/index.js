614764c1bda9fed7265f51984e53b8f479350127
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-06 16:00:54 -0500

    Getting the fields of the request body as an array

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -43,6 +43,7 @@ app.get('/users/:id', async (request, response) => {
 });
 
 app.patch('/users/:id', async (request, response) => {
+    const updates = Object.keys(request.body);
     try {
         const user = await User.findByIdAndUpdate(request.params.id, request.body, {
             new: true,
