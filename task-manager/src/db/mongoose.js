d01d188c570b6004dc6ceeb5fce4636f515034f5
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-06 16:59:59 -0500

    Configuration to avoid deprecated use of background findAndModify

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/task-manager-api', {
     useNewUrlParser: true,
-    useCreateIndex: true
-    //useFindAndModify: false
+    useCreateIndex: true,
+    useFindAndModify: false
 });
