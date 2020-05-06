3bd1aa73721a9ac1b98e0eb80ce9898e19bf70fc
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-06 18:13:45 -0500

    Refactoring to move task router operations

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
 const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,79 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(userRouter);
-
-
-
-app.post('/tasks', async (request, response) => {
-    const task = new Task(request.body);
-
-    try {
-        task.save();
-        response.status(201).send(task);
-    } catch (error) {
-        response.status(400).send(error);
-    }
-});
-
-app.get('/tasks', async (request, response) => {
-    try {
-        let allTasks = await Task.find();
-        const count = await Task.countDocuments();
-        allTasks.push(count);
-        response.send(allTasks);
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
-
-app.get('/tasks/:id', async (request, response) => {
-    try {
-        const task = await Task.findById(request.params.id);
-        if(!task){
-            return response.status(404).send();
-        }
-        response.send(task);
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
-
-app.patch('/tasks/:id', async (request, response) => {
-    const updates = Object.keys(request.body);
-    const allowedUpdates = ['description', 'completed'];
-
-    const isValidUpdating = updates.every(update => allowedUpdates.includes(update));
-    
-    if(!isValidUpdating) {
-        return response.status(400).send({ error: 'Invalid update!' });
-    }
-
-    try {
-        const task = await Task.findByIdAndUpdate(request.params.id, request.body, {
-            new: true,
-            runValidators: true
-        });
-        if(!task) {
-            return response.status(404).send();
-        }
-        response.send(task);
-    } catch (error) {
-        response.status(400).send(error);
-    }
-});
-
-app.delete('/tasks/:id', async (request, response) => {
-    try {
-        const task = await Task.findByIdAndDelete(request.params.id);
-
-        if(!task) {
-            return response.status(404).send();
-        }
-        response.send(task);
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
+app.use(taskRouter);
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
---
+++ task-manager/src/routers/task.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+const Task = require('../models/task');
+
+router.post('/tasks', async (request, response) => {
+    const task = new Task(request.body);
+
+    try {
+        task.save();
+        response.status(201).send(task);
+    } catch (error) {
+        response.status(400).send(error);
+    }
+});
+
+router.get('/tasks', async (request, response) => {
+    try {
+        let allTasks = await Task.find();
+        const count = await Task.countDocuments();
+        allTasks.push(count);
+        response.send(allTasks);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+router.get('/tasks/:id', async (request, response) => {
+    try {
+        const task = await Task.findById(request.params.id);
+        if(!task){
+            return response.status(404).send();
+        }
+        response.send(task);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+router.patch('/tasks/:id', async (request, response) => {
+    const updates = Object.keys(request.body);
+    const allowedUpdates = ['description', 'completed'];
+
+    const isValidUpdating = updates.every(update => allowedUpdates.includes(update));
+    
+    if(!isValidUpdating) {
+        return response.status(400).send({ error: 'Invalid update!' });
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(request.params.id, request.body, {
+            new: true,
+            runValidators: true
+        });
+        if(!task) {
+            return response.status(404).send();
+        }
+        response.send(task);
+    } catch (error) {
+        response.status(400).send(error);
+    }
+});
+
+router.delete('/tasks/:id', async (request, response) => {
+    try {
+        const task = await Task.findByIdAndDelete(request.params.id);
+
+        if(!task) {
+            return response.status(404).send();
+        }
+        response.send(task);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+module.exports = router;
