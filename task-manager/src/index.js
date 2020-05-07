56fa7e1c4bd7408c0cdb6683888b6fb14ddef51c
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-07 18:59:08 -0500

    Logging method and path of request

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,7 +6,9 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use((request, response, next) => {});
+app.use((request, response, next) => {
+    console.log(request.method, request.path);
+});
 
 app.use(express.json());
 
