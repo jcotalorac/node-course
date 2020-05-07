0b1c69e9043604f1637fccf61eb7d6877f798ce9
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-06 21:40:47 -0500

    Correcting creation of model schema for user

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -38,7 +38,9 @@ const userSchema = {
             }
         }
     }
-};
+});
+
+userSchema.pre();
 
 const User = mongoose.model('User', userSchema);
 
