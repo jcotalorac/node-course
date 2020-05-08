6de9803c56be4894c946a48c2129bf1393c1e6a6
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-08 16:29:33 -0500

    Getting the id of owner

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,3 +14,12 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
+
+const Task = require('./models/task');
+
+const main = async () => {
+    const task = await Task.findById('5eb5c79187d63462d016335b');
+    console.log(task.owner);
+};
+
+main();
