678cc0621f9ff9ef4d893894cdc28240d6ee47e1
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-07 23:16:58 -0500

    Saving authenticated token in request for succesive access

--- task-manager/src/middleware/auth.js
+++ task-manager/src/middleware/auth.js
@@ -13,6 +13,7 @@ const auth = async (request, response, next) => {
         if(!user) {
             throw new Error();
         }
+        request.token = token;
         request.user = user;
         next();
     } catch (error) {
