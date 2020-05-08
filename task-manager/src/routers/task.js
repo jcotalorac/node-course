d00c6dfb0dba5f1f325e3afc8db9fefaaed02428
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-08 18:18:27 -0500

    Authenticated version to update a task

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -68,7 +68,31 @@ router.get('/tasks/:id', auth, async (request, response) => {
     }
 });
 
-router.patch('/tasks/:id', async (request, response) => {
+// router.patch('/tasks/:id', async (request, response) => {
+//     const updates = Object.keys(request.body);
+//     const allowedUpdates = ['description', 'completed'];
+
+//     const isValidUpdating = updates.every(update => allowedUpdates.includes(update));
+    
+//     if(!isValidUpdating) {
+//         return response.status(400).send({ error: 'Invalid update!' });
+//     }
+
+//     try {
+//         const task = await Task.findById(request.params.id);
+//         if(!task) {
+//             return response.status(404).send();
+//         }
+//         updates.forEach(update => task[update] = request.body[update]);
+//         await task.save();
+
+//         response.send(task);
+//     } catch (error) {
+//         response.status(400).send(error);
+//     }
+// });
+
+router.patch('/tasks/:id', auth, async (request, response) => {
     const updates = Object.keys(request.body);
     const allowedUpdates = ['description', 'completed'];
 
@@ -79,7 +103,10 @@ router.patch('/tasks/:id', async (request, response) => {
     }
 
     try {
-        const task = await Task.findById(request.params.id);
+        const task = await Task.findOne({
+            _id: request.params.id,
+            owner: request.user._id
+        });
         if(!task) {
             return response.status(404).send();
         }
