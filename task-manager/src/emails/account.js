1a18485640d098b24abc0dda9820c8742c192bf4
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-09 23:13:35 -0500

    Sending mail demo

--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -1,2 +1,13 @@
-const sendgridAPIKey = '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';
+const sgMail = require('@sendgrid/mail');
+//const sendgridAPIKey = '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';
+const sendgridAPIKey = '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';
 
+sgMail.setApiKey(sendgridAPIKey);
+const msg = {
+    to: 'jcotalorac@gmail.com',
+    from: 'juancagado@hotmail.com',
+    replyTo: 'juancagado@hotmail.com',
+    subject: 'This is my first creation',
+    text: 'I hope this one actually gets to you.'
+};
+sgMail.send(msg);
