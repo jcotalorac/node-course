1b81a5da595871f5d776119fa8268f95607001e7
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-12 22:04:52 -0500

    Async function disposable for test cases

--- task-manager/src/math.js
+++ task-manager/src/math.js
@@ -8,8 +8,17 @@ const celsiusToFahrenheit = (temp) => {
     return (temp * 1.8) + 32;
 };
 
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b);
+        }, 2000);
+    });
+};
+
 module.exports = {
     calculateTip,
     fahrenheitToCelsius,
-    celsiusToFahrenheit
+    celsiusToFahrenheit,
+    add
 };
