1b18ac34d62787b26b4a067795bee4c5b267f987
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-13 00:02:15 -0500

    Before and after running of each test

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,10 +1,18 @@
 const request = require('supertest');
 const app = require('../src/app');
 
+beforeEach(() => {
+    console.log('beforeEach');
+});
+
+afterEach(() => {
+    console.log('afterEach');
+});
+
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
         name: "A name",
-        email: "mail@mail.com",
+        email: "jcotalorac@gmail.com",
         password: "pwd12345"
     }).expect(201);
 });
