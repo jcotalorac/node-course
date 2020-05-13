8bf3aa456ebff9e1c5de06aeec38fe9cb4a757bc
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-13 18:30:38 -0500

    Setting task data preparement for task test suite

--- task-manager/tests/fixtures/db.js
+++ task-manager/tests/fixtures/db.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const moongose = require('mongoose');
 const User = require('../../src/models/user');
+const Task = require('../../src/models/task');
 
 const userOneId = new moongose.Types.ObjectId();
 const userOne = {
@@ -13,9 +14,46 @@ const userOne = {
     }]
 };
 
+const userTwoId = new moongose.Types.ObjectId();
+const userTwo = {
+    _id: userTwoId,
+    name: "User two",
+    email: "mail@mail.com",
+    password: "twopwd123",
+    tokens: [{
+        token: jwt.sign({ _id: userTwoId}, process.env.JWT_SECRET)
+    }]
+};
+
+const taskOne = {
+    _id: new moongose.Types.ObjectId(),
+    description: "First task",
+    completed: false,
+    owner: userOne._id
+};
+
+const taskTwo = {
+    _id: new moongose.Types.ObjectId(),
+    description: "Second task ",
+    completed: true,
+    owner: userOne._id
+};
+
+const taskThree = {
+    _id: new moongose.Types.ObjectId(),
+    description: "Third task ",
+    completed: true,
+    owner: userTwo._id
+};
+
 const setupDatabase = async () => {
     await User.deleteMany();
+    await Task.deleteMany();
     await new User(userOne).save();
+    await new User(userTwo).save();
+    await new Task(taskOne).save();
+    await new Task(taskTwo).save();
+    await new Task(taskThree).save();
 };
 
 module.exports = {
