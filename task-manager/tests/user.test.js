e3aaacbc13eee365e0ec6803aa23c78150e28615
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-12 23:45:28 -0500

    Success test case for creating user

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,1 +1,10 @@
 const request = require('supertest');
+const app = require('../src/app');
+
+test('Should signup a new user', async () => {
+    await request(app).post('/users').send({
+        name: "A name",
+        email: "mail@mail.com",
+        password: "pwd12345"
+    }).expect(201);
+});
