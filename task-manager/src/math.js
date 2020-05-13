4862b7b24703bc8843380ea7d276eb7ceb352e3c
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-12 21:16:13 -0500

    Test as an assurance of refactoring

--- task-manager/src/math.js
+++ task-manager/src/math.js
@@ -1,7 +1,4 @@
-const calculateTip = (total, tipPercent) => {
-    const tip = total * tipPercent + total;
-    return total + tip;
-};
+const calculateTip = (total, tipPercent) => total + (total * tipPercent);
 
 module.exports = {
     calculateTip
