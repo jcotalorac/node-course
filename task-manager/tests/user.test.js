76c128709451dff226e2b0753b407a23ce9ff5d6
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-13 10:01:13 -0500

    Test case for delete user profile

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -46,3 +46,7 @@ test('Should get profile user', async() => {
 test('Should not get profile for unauthenticades user', async() => {
     await request(app).get('/users/me').send().expect(401);
 });
+
+test('Should delete account for user', async() => {
+    await request(app).delete('/users/me').set('Authorization', 'Bearer ' + userOne.tokens[0].token).send().expect(200);
+});
