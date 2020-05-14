f34c43931c5eef3ad514dc6686c5341adc7a53d1
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-14 11:25:06 -0500

    Refactor to use http server with express app configuration

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log('Running on port ' + port);
 });
