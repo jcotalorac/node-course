19260e3874b19c662173b9dc2d3b97b898189060
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-14 11:26:51 -0500

    Instance of socketio

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio();
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
