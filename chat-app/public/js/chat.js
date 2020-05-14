a972cc67dbadd7ce513ad5bb0a57c110e79a5010
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-14 14:51:44 -0500

    Maintaining reference code

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,10 +1,10 @@
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated!', count);
-});
+// socket.on('countUpdated', (count) => {
+//     console.log('The count has been updated!', count);
+// });
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('Clicked');
-    socket.emit('increment');
-});
+// document.querySelector('#increment').addEventListener('click', () => {
+//     console.log('Clicked');
+//     socket.emit('increment');
+// });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,19 +12,19 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
+// let count = 0;
 
-io.on('connection', (socket) => {
-    console.log('New WebSocket connection');
+// io.on('connection', (socket) => {
+//     console.log('New WebSocket connection');
 
-    socket.emit('countUpdated', count);
+//     socket.emit('countUpdated', count);
     
-    socket.on('increment', () => {
-        count++;
-        //socket.emit('countUpdated', count);
-        io.emit('countUpdated', count);
-    });
-});
+//     socket.on('increment', () => {
+//         count++;
+//         //socket.emit('countUpdated', count);
+//         io.emit('countUpdated', count);
+//     });
+// });
 
 
 server.listen(port, () => {
