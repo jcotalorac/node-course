601b75caa598a89415961497d426e780b85425c6
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-14 18:46:53 -0500

    On the same user connection, when is disconnected, send broadcast to the still connected clients

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -35,6 +35,10 @@ io.on('connection', (socket) => {
     socket.on('sendMessage', (message) => {
         io.emit('message', message);
     });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left');
+    });
 });
 
 
