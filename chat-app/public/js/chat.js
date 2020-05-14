47aae21428911b3348eff97f25b68bb55040d28f
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-14 14:11:57 -0500

    Creating increment button

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -3,3 +3,7 @@ const socket = io();
 socket.on('countUpdated', (count) => {
     console.log('The count has been updated!', count);
 });
+
+document.querySelector('#increment').addEventListener('click', () => {
+    console.log('Clicked');
+});
