0ee09fb2dbccc40daf3e0a14cf3fbe3ee003e065
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-14 19:59:39 -0500

    Sharing google maps query for location broadcast

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -41,7 +41,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('sendLocation', (location) => {
-        io.emit('message', `Location: ${location.latitude}, ${location.longitude}`);
+        io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`);
     });
 });
 
