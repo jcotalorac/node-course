281f013bfe6215f891f51114aa3d604c36527d15
juan.otalora <juancarlos.otalora@wnco.com>
2020-05-15 18:41:54 -0500

    Tracking of users and rooms, adding

---
+++ chat-app/src/utils/users.js
@@ -0,0 +1,26 @@
+const users = [];
+
+const addUser = ({ id, username, room }) => {
+    username = username.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    if(!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        };
+    }
+
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username;
+    });
+
+    if(existingUser) {
+        return {
+            error: 'Username is in use!'
+        };
+    }
+
+    const user = { id, username, room };
+    users.push(user);
+    return { user };
+};
